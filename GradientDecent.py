119b76c1834a7b47567d097060da6353bf9efaa8
quocvinh2014 <bui.quoc.vinh@hotmail.com>
2016-12-17 21:17:43 +0700

    Update normalize

--- GradientDecent.py
+++ GradientDecent.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-temporary = np.array([0.1,0.1,0.1])
+temporary = np.array([1,1,1])
 theta = temporary
 
 def gradient_decent(theta, targets, data, learning_rate):
     theta_temp = temporary
     for i in range(0, len(targets)):
-        predict = theta[0] + theta[1] * data[i][0] + theta[2] * data[i][1]
+        predict = theta[0] + (theta[1] * data[i][0]) + (theta[2] * data[i][1])
         error = targets[i] - predict
 
         theta_temp[0] = theta[0] + learning_rate * error
@@ -16,8 +16,13 @@ def gradient_decent(theta, targets, data, learning_rate):
 
 def run():
     points_x = np.genfromtxt('ex3x.dat', delimiter='   ')
+    #normalize
+    length = len(points_x)
+    for i in range(0,len(points_x)):
+        points_x[i][0] = points_x[i][0] / 1000
+
     points_y = np.genfromtxt('ex3y.dat')
-    gradient_decent(theta, points_y, points_x, 0.000000001)
+    gradient_decent(theta, points_y, points_x, 0.05)
     print(theta)
 
 run()
